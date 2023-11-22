077c76255642bde2fd16fee66a96ad4e88295d4d
디지털디자인 <50073413+pam7461@users.noreply.github.com>
2023-11-22 16:45:06 +0900

    Add files via upload

--- js/function.js
+++ js/function.js
@@ -1,4 +1,31 @@
 //============ 함수 ============
+ 
+  function fnAnimation(elArray){
+    let scrTop = document.querySelector('html').scrollTop;
+    let winHeight = window.innerHeight;
+
+    for(let idx=0;idx<=elArray.length-1;idx++){
+      let elArrayHeight = elArray[idx].offsetHeight;
+      let absElArray = window.pageYOffset + elArray[idx].getBoundingClientRect().top;
+      let showTop = absElArray - winHeight + elArrayHeight / 2;
+      // let showTop = absElArray - winHeight;
+      if(scrTop>=showTop){
+        elArray[idx].classList.add('active');
+      }else{
+        elArray[idx].classList.remove('active');
+      }        
+    }
+  }
+
+  function fnHeader(elHeader){
+    let scrTop = document.querySelector('html').scrollTop;
+    if(scrTop>=100){
+      elHeader.classList.add('active');
+    }else{
+      elHeader.classList.remove('active');
+    }
+  }
+
   // 비디오를 뒤로 보내는 함수
   function videoBack(){
     $('.media-box').removeClass('active');
@@ -8,10 +35,10 @@
     el.addClass('active');
   }
   // 비디오 멈춤 함수
-  function videoStop(){      
-    $('video')[0].pause();
-    $('video')[1].pause();
-    $('video')[2].pause();
+  function videoStop(){          
+      $('video')[0].pause();
+      $('video')[1].pause();
+      $('video')[2].pause();
     $('#btn-video').text('재생');
   }
   // 비디오 재생 함수
@@ -34,5 +61,7 @@
     $('video').prop('loop',false);
     $('.active video').prop('loop',true);      
   }
-    
+   
+  
+  
     
