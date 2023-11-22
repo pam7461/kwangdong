2ee22317e57a81704175e6e8c7f31ffebab3fe8c
디지털디자인 <50073413+pam7461@users.noreply.github.com>
2023-11-22 18:10:41 +0900

    Add files via upload

--- js/function.js
+++ js/function.js
@@ -16,7 +16,6 @@
       }        
     }
   }
-
   function fnHeader(elHeader){
     let scrTop = document.querySelector('html').scrollTop;
     if(scrTop>=100){
@@ -25,7 +24,6 @@
       elHeader.classList.remove('active');
     }
   }
-
   // 비디오를 뒤로 보내는 함수
   function videoBack(){
     $('.media-box').removeClass('active');
@@ -60,6 +58,46 @@
   function videoLoop(){
     $('video').prop('loop',false);
     $('.active video').prop('loop',true);      
+  }
+  // 즉시실행함수
+  function fnBorder(){
+    for(let cnt=1; cnt<=4; cnt++){
+      $('.div-con-art').prepend('<span></sapn>');
+    }
+    $('.div-con-art span').css({
+      position:'absolute',      
+      background:'#ff0000',
+    })
+    for(let idx=0;idx<=$('.div-con-art').length-1;idx++){
+      $('.div-con-art').eq(idx).children('span').eq(0).css({
+        left:'0',
+        top:'0',
+        width:'0',
+        // width:'100%',
+        height:'5px',
+      })
+      $('.div-con-art').eq(idx).children('span').eq(1).css({
+        right:'0',
+        top:'0',      
+        width:'5px',
+        height:'0',
+        // height:'100%',
+      })
+      $('.div-con-art').eq(idx).children('span').eq(2).css({
+        right:'0',
+        bottom:'0',
+        width:'0',
+        // width:'100%',
+        height:'5px',
+      })
+      $('.div-con-art').eq(idx).children('span').eq(3).css({
+        left:'0',
+        bottom:'0',
+        width:'5px',
+        height:'0',
+        // height:'100%',
+      })
+    }
   }
    
   
